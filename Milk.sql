30b3ba54e0d0612ff5e2b7959a5d53ebd2d7c57b
bu256 <bulostm@gmail.com>
2016-04-08 14:08:31 +0300

    first

---
+++ Milk.sql
@@ -0,0 +1,9 @@
+п»ї
+select Distinct c.* from CustomerTable as c, PurchaiseTable as p
+where c.CustomerId=p.CustomerId and p.ProductName='Молоко' 
+and c.CustomerId NOT IN
+(
+	select CustomerId from PurchaiseTable 
+	where ProductName='Сметана'
+	and PurchaiseDatetime >= (select GETDATE() - 1 month)
+)
